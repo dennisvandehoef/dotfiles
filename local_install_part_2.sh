22f17d3fb4572bbf86e114ff64c4502ec7e0a17a
Dennis van de Hoef <dennis.vandehoef@kaeuferportal.de>
2014-10-02 22:20:47 +0200

    Update local_install_part_2.sh

--- local_install_part_2.sh
+++ local_install_part_2.sh
@@ -1,3 +1,7 @@
 echo -e "\n\n\n\n\nCreating git reposetory\n\n\n\n\n"
 mkdir ~/dotfiles
 git clone git@github.com:dennisvandehoef/dotfiles.git ~/dotfiles
+
+echo -e "\n\n\n\n\nConfig VIM\n\n\n\n\n"
+cp -r ~/.vim ~/.vim.old 2>/dev/null
+cp -r ~/.vimrc ~/.vimrc.old 2>/dev/null
