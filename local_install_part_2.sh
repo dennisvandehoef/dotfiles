d2937a242992d2477bbbe3f0c956fbdf02680ece
Dennis van de Hoef <dennis.vandehoef@kaeuferportal.de>
2014-10-02 22:30:09 +0200

    Update local_install_part_2.sh

--- local_install_part_2.sh
+++ local_install_part_2.sh
@@ -5,3 +5,8 @@ git clone git@github.com:dennisvandehoef/dotfiles.git ~/dotfiles
 echo -e "\n\n\n\n\nConfig VIM\n\n\n\n\n"
 cp -r ~/.vim ~/.vim.old 2>/dev/null
 cp -r ~/.vimrc ~/.vimrc.old 2>/dev/null
+rm -fr ~/.vim 2>/dev/null
+rm -f ~/.vimrc 2>/dev/null
+
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
