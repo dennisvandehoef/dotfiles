0a389eb661390eb7e17222fa5570429e2977c568
Dennis van de Hoef <dennis.vandehoef@kaeuferportal.de>
2014-10-02 20:58:12 +0200

    Add comments to install

--- local_install.sh
+++ local_install.sh
@@ -1,25 +1,29 @@
+echo -e "\n\n\n\n\nUpdating the system\n\n\n\n\n"
 update
 
-#prepare MongoDB
+echo -e "\n\n\n\n\nprepare for MongoDB\n\n\n\n\n"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
-#basic prepare
+echo -e "\n\n\n\n\nUpdate apt-get\n\n\n\n\n"
 sudo apt-get update
 
-#installs
+echo -e "\n\n\n\n\nInstalling software\n\n\n\n\n"
 sudo apt-get install mysql-server
 sudo apt-get install -y mongodb-org
 sudo apt-get install git
 sudo apt-get install tree
 sudo apt-get install unrar
 
+echo -e "\n\n\n\n\nInstalling Ruby\n\n\n\n\n"
 sudo apt-get install ruby-rvm
 rvm install ruby --latest
 sudo gem update --system
 sudo gem install rails
 
-#cleanup
+echo -e "\n\n\n\n\nCleanup\n\n\n\n\n"
 sudo apt-get autoremove
 sudo apt-get clean
 sudo apt-get autoclean
+
+echo -e "\n\n\n\n\nDone :)"
