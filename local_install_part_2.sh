6b4823b023bd7c159e6bd1aa5565dc1947c42af1
Dennis van de Hoef <dennisvdhoef@gmail.com>
2015-07-09 22:14:06 +0200

    remove empty lines

--- local_install_part_2.sh
+++ local_install_part_2.sh
@@ -19,7 +19,6 @@ ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim /usr/local/bin/vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
 
-
 echo -e "\n\n\n\n\nConfig GIT\n\n\n\n\n"
 cp -r ~/.gitconfig ~/.gitconfig.old 2>/dev/null
 cp -r ~/.gitignore_global ~/.gitignore_global.old 2>/dev/null
@@ -32,16 +31,12 @@ ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 
 echo -e "\n\n\n\n\nConfig aliases\n\n\n\n\n"
 cp -r ~/.bash_aliases ~/.bash_aliases.old 2>/dev/null
-
 rm -f ~/.bash_aliases 2>/dev/null
-
 ln -s ~/dotfiles/aliases ~/.bash_aliases
 #mac @ work
 echo '[[ -s "$HOME/.bash_aliases" ]] && source "$HOME/.bash_aliases"' >> ~/.bash_profile
 
 echo -e "\n\n\n\n\nConfig Gem\n\n\n\n\n"
 cp -r ~/.gemrc ~/.gemrc.old 2>/dev/null
-
 rm -f ~/.gemrc 2>/dev/null
-
 ln -s ~/dotfiles/gemrc ~/.gemrc
