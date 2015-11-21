c4181ab2c84a4a23c914f0fd7040b648a3f04d70
Dennis van de Hoef <dennisvdhoef@gmail.com>
2015-11-21 20:20:42 +0100

    remove mongo from local set

--- local_install.sh
+++ local_install.sh
@@ -1,13 +1,8 @@
-echo -e "\n\n\n\n\nprepare for MongoDB\n\n\n\n\n"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-
 echo -e "\n\n\n\n\nUpdate apt-get\n\n\n\n\n"
 sudo apt-get update
 
 echo -e "\n\n\n\n\nInstalling software\n\n\n\n\n"
 sudo apt-get install -y mysql-server
-sudo apt-get install -y mongodb-org
 sudo apt-get install -y git
 sudo apt-get install -y tree
 sudo apt-get install -y bundler
