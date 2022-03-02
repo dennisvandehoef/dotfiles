21a6fbef1cfec2dd756040af6518e5a64ad30185
Dennis van de Hoef <dennis.vandehoef@aroundhome.de>
2022-03-02 10:55:16 +0100

    Update coloring, and fix to one strings

--- prompt_helpers.sh
+++ prompt_helpers.sh
@@ -25,15 +25,18 @@ __colored_string() {
   local color="214" # orange
 
   count=$(echo $input | xxd -p | sed -E "s/[a-z]//g")
+  count=$(echo ${count: -19})
+
   while [ 231 -le $count ]
   do
-    count=$(echo $((count / 15)))
+    count=$(echo $((count / 87)))
+
+    # colors unreadable on black bg: 0 16 17 18 19 23 24 52 53 54 55 88 89
+    if [ $count = "0" ] || [ $count = "16" ] || [ $count = "17" ] || [ $count = "18" ] || [ $count = "19" ] || [ $count = "23" ] || [ $count = "24" ] || [ $count = "52" ] || [ $count = "53" ] || [ $count = "54" ] || [ $count = "55" ] || [ $count = "88" ] || [ $count = "89" ]; then
+      count=$(echo $((count + count + 15)))
+    fi
   done
 
-  # # colors unreadable on black bg: 52 16 17 18 0
-  # if [$count = "0"] || [$count = "16"] || [$count = "17"] || [$count = "18"] || [$count = "52"]; then
-  #   count=$(echo $((count + 10)))
-  # fi
   color="000$count"
   color=$(echo ${color: -3})
 
